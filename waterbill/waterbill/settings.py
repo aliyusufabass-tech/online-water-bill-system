0140ca3ed01a5edef3d3c610b7af5ebb9bb5441b
Yusuf Abass Ali <aliyusufabass@gmail.com>
2026-02-20 01:01:41 -0800

    fullprojectcommit2

--- waterbill/waterbill/settings.py
+++ waterbill/waterbill/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'waterbill.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jitunene',
+        'USER': 'postgres',
+        'PASSWORD': 'Yusuf@24',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
